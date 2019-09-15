9563946f827bb541f00d1d6bcf4ed60ce4a6fa51
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-09-14 23:24:09 -0700

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friendMatch = [
+  {
+    name: "Samuel L. Jackson",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQgw99LSQsAO6JEewbkmin7FP3CQYjNavjKEF5QTpzmh7VwZ-l",
+    scores: ["2", "2", "2", "2", "2"]
+  },
+  {
+    name: "Captain Obvious",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV9v_vDh-d3TB9aZ3A1hiFh0BiyprcFpv0kJdvPLyxoTj4n085",
+    scores: ["3", "3", "3", "3", "3"]
+  },
+  {
+    name: "Morgan Freeman",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZyv8iNNwEGtBGIwdJ9ZNS31p-noUFps11LrVakN0UCryalXi6XQ ",
+    scores: ["1", "1", "1", "1", "1"]
+  },
+  {
+    name: "Borat",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWHFSfmT9owWfv2LnyOZ4lm4BUx1mdFsFPGTVflrOaVUaemH7E ",
+    scores: ["3", "3", "3", "3", "1"]
+  }
+];
+
+module.exports = friendMatch;
